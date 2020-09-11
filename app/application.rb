c7c0aef91f0c240304808e8e2d06365421f28cfe
Olivia Cubela <oliviacubela@gmail.com>
2020-09-11 18:12:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,4 +28,12 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def empty_cart
+    if @@items.empty?
+      return "Your cart is empty"
+    else
+      @@items
+    end
+  end
 end
