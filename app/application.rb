458383ad20bc2cf9b707f17147e3836b48d2ed16
Olivia Cubela <oliviacubela@gmail.com>
2020-09-11 19:58:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-  
+
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
